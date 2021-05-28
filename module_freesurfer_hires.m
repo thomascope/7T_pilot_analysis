4158a082b93c9a93a136b483200b4716d0a9f3ff
thomascope <thomascope@gmail.com>
2021-05-28 12:33:34 +0100

    Created an imfilled mask for freesurfer from the SPM segmentation to avoid white matter holes

--- module_freesurfer_hires.m
+++ module_freesurfer_hires.m
@@ -1,14 +1,13 @@
 function module_freesurfer_hires(preprocessedpathstem,subjects,crun,rawdatafolder,scanname,scriptdir,overwrite,skullstripped)
-%Recon-all with hires flag and mris_inflate -n 30 on already skullstripped
-%structural with csf retained
-% this_scan(crun) = cellstr([rawpathstem basedir{crun} '/' fullid{crun} '/' blocksin_folders{crun}{find(strcmp(blocksout{crun},'structural'))} '/' blocksin{crun}{find(strcmp(blocksout{crun},'structural'))}]);
-% this_scan(crun) = cellstr([preprocessedpathstem subjects{crun} '/structural_csf.nii']);
+%Recon-all with hires flag
 
 this_scan(crun) = cellstr([rawdatafolder '/' scanname]);
-if skullstripped
+if skullstripped == 1
    this_subjects_dir = [preprocessedpathstem '/freesurfer_skullstripped/']; 
-else
+elseif skullstripped == 0
     this_subjects_dir = [preprocessedpathstem '/freesurfer/']; 
+elseif skullstripped == 2
+    this_subjects_dir = [preprocessedpathstem '/freesurfer_masked/']; 
 end
 
 %pathstem = [preprocessedpathstem subjects{crun}];
@@ -22,13 +21,12 @@ end
 setenv('SUBJECTS_DIR',this_subjects_dir);
 setenv('FSF_OUTPUT_FORMAT','nii');
 
-if skullstripped == 0
+if skullstripped == 0 % Let Freesurfer skullstrip - default but fails more than a third of the time with PINFA 7T dataset
     %cmd = ['recon-all -all -s ' subjects{crun} ' -hires -i ' this_scan{crun} ' -notal-check -cw256 -expert ' scriptdir 'expert_hires.opts'];
     cmd = ['recon-all -all -s ' subjects{crun} ' -hires -i ' this_scan{crun} ' -notal-check -cw256'];
     fprintf(['Submitting the following command: ' cmd]);
     system(cmd);
-else
-    %error('Skullstripped version not yet implemented - see recipe below')
+elseif skullstripped == 1 % Use the SPM skullstripped image with csf as the input - works every time but has holes in the white matter that interfere with temporal lobe.
     % Q. I have already skull-stripped data. Can I submit it to recon-all?
     % A: If your skull-stripped volume does not have the cerebellum, then no. If it does, then yes, however you will have to run the data a bit differently.
     % First you must run only -autorecon1 like this:
@@ -45,4 +43,23 @@ else
     cmd = ['recon-all -autorecon2 -autorecon3 -noskullstrip -s ' subjects{crun} ' -hires -notal-check -cw256'];
     fprintf(['Submitting the following second stage command: ' cmd]);
     system(cmd);
+elseif skullstripped == 2 % Create an imfilled mask for freesurfer from the SPM segmentation, which must have been run previously, and use this to skullstrip the input
+    % Q. I have already skull-stripped data. Can I submit it to recon-all?
+    % A: If your skull-stripped volume does not have the cerebellum, then no. If it does, then yes, however you will have to run the data a bit differently.
+    % First you must run only -autorecon1 like this:
+    % recon-all -autorecon1 -noskullstrip -s <subjid>
+    cmd = ['recon-all -autorecon1 -noskullstrip -s ' subjects{crun} ' -hires -i ' this_scan{crun} ' -notal-check -cw256 -bigventricles'];
+    fprintf(['Submitting the following first stage command: ' cmd]);
+    system(cmd);
+    
+    % Then you will have to make a symbolic link or copy T1.mgz to brainmask.auto.mgz and a link from brainmask.auto.mgz to brainmask.mgz. Finally, open this brainmask.mgz file and check that it looks okay (there is no skull, cerebellum is intact; use the sample subject bert that comes with your FreeSurfer installation to make sure it looks comparable). From there you can run the final stages of recon-all:
+    % recon-all -autorecon2 -autorecon3 -s <subjid>
+    data_dir = [this_subjects_dir subjects{crun}];
+    copyfile([data_dir '/mri/T1.mgz'],[data_dir '/mri/brainmask.auto.mgz'])
+    copyfile([data_dir '/mri/T1.mgz'],[data_dir '/mri/brainmask.mgz'])
+    cmd = ['recon-all -autorecon2 -autorecon3 -noskullstrip -s ' subjects{crun} ' -hires -notal-check -cw256 -bigventricles'];
+    fprintf(['Submitting the following second stage command: ' cmd]);
+    system(cmd);
+else
+    error('Unknown skullstrip argument specified')
 end
--- scratch_freesurfer.m
+++ scratch_freesurfer.m
@@ -1,28 +1,62 @@
+
+skullstripped = 2;  
+coreg_and_crop_images = 0; %Necessary, but can skip if you've done before
+overwrite = 0; % If you want to re-do a step - otherwise will crash if data already exist.
 parfor crun = 1:nrun
-   coreg_and_crop_images = 0; %Necessary, but can skip if you've done before
-%     rawdatafolder = [rawpathstem basedir{crun} '/' fullid{crun} '/' blocksin_folders{crun}{find(strcmp(blocksout{crun},'structural'))}];
-%     scanname = blocksin{crun}{find(strcmp(blocksout{crun},'structural'))};
-%     skullstripped = 0;
-    rawdatafolder = [preprocessedpathstem subjects{crun}];
-    scanname = 'structural_csf.nii';
-    skullstripped = 1;
-    overwrite = 0
     setenv('FREESURFER_HOME','/home/tc02/freesurfer'); %Freesurfer 7.1.0
     !csh /home/tc02/freesurfer/SetUpFreeSurfer.csh
-    
-    this_scan = cellstr([rawdatafolder '/' scanname]);
-    if coreg_and_crop_images
-        addpath('./crop')
-        crop_images(this_scan) % Vital to do rough Talaraiching first as Freesurfer can't manage it.
+    setenv('FSF_OUTPUT_FORMAT','nii');
+    if skullstripped == 0 % Let Freesurfer skullstrip - fails more than a third of the time
+        rawdatafolder = [rawpathstem basedir{crun} '/' fullid{crun} '/' blocksin_folders{crun}{find(strcmp(blocksout{crun},'structural'))}];
+        scanname = blocksin{crun}{find(strcmp(blocksout{crun},'structural'))};
+        this_scan = cellstr([rawdatafolder '/' scanname]);
+        if coreg_and_crop_images
+            addpath('./crop')
+            crop_images(this_scan) % Vital to do rough Talaraiching first as Freesurfer can't manage it.
+        end
+        scanname = ['p' scanname];
+        
+    elseif skullstripped == 1 % Use the SPM skullstripped image with csf as the input - works every time but has holes in the white matter that interfere with temporal lobe.
+        rawdatafolder = [preprocessedpathstem subjects{crun}];
+        scanname = 'structural_csf.nii';
+        this_scan = cellstr([rawdatafolder '/' scanname]);
+        if coreg_and_crop_images
+            addpath('./crop')
+            crop_images(this_scan) % Vital to do rough Talaraiching first as Freesurfer can't manage it.
+        end
+        scanname = ['p' scanname];
+        
+    elseif skullstripped == 2 % Create an imfilled mask for freesurfer from the SPM segmentation, which must have been run previously
+        rawdatafolder = [rawpathstem basedir{crun} '/' fullid{crun} '/' blocksin_folders{crun}{find(strcmp(blocksout{crun},'structural'))}];
+        scanname = blocksin{crun}{find(strcmp(blocksout{crun},'structural'))};
+        this_scan = cellstr([rawdatafolder '/' scanname]);
+        if coreg_and_crop_images
+            addpath('./crop')
+            crop_images(this_scan) % Vital to do rough Talaraiching first as Freesurfer can't manage it.
+        end
+        all_coreged_vols = [];
+        for tissue_class = 1:3
+            vol1 = spm_vol('DATA_0021.nii');
+            vol2 = spm_vol(['c' num2str(tissue_class) 'DATA_0021.nii']);
+            new_vol = vol2;
+            new_vol.fname = ['coreg_' vol2.fname];
+            new_vol.mat = vol1.mat;
+            spm_write_vol(new_vol,spm_read_vols(vol2));
+            all_coreged_vols = [all_coreged_vols; 'coreg_c' num2str(tissue_class) 'DATA_0021.nii'];
+        end
+        Vo = spm_imcalc(all_coreged_vols,'coreg_brainmask.nii','i1+i2+i3>0')
+        temp = spm_read_vols(spm_vol(Vo));
+        filled_mask = imfill(temp,'holes');
+        spm_write_vol(spm_vol('coreg_brainmask.nii'),filled_mask);
+        spm_imcalc(char({scanname;'coreg_brainmask.nii'}),'coreg_filled_skullstripped.nii','i1.*i2');
+        scanname = 'coreg_filled_skullstripped.nii';
     end
-    scanname = ['p' scanname];
-    
     module_freesurfer_hires(preprocessedpathstem,subjects,crun,rawdatafolder,scanname,scriptdir,overwrite,skullstripped)
     
 end
 
 % Now manually view outputs and decide which ones are bad
-output_folder = [preprocessedpathstem '/freesurfer/'];
+output_folder = [preprocessedpathstem '/freesurfer/']; % For skullstripped == 0
 bad_subjects = {    
     'P7P01'
     'P7P02'
@@ -41,6 +75,9 @@ bad_subjects = {
     'P7C22'
     };
 
+%bad_subjects = {};
+output_folder = [preprocessedpathstem '/freesurfer_skullstripped/']; % For skullstripped == 1
+output_folder = [preprocessedpathstem '/freesurfer_masked/']; % For skullstripped == 2
 if isempty(bad_subjects)
     for crun = 1:nrun
         cmd = ['freeview -v ' output_folder subjects{crun} '/mri/T1.mgz ' output_folder subjects{crun} '/mri/wm.mgz ' output_folder subjects{crun} '/mri/brainmask.mgz ' output_folder subjects{crun} '/mri/aseg.mgz:colormap=lut:opacity=0.2 -f ' output_folder subjects{crun} '/surf/lh.white:edgecolor=blue ' output_folder subjects{crun} '/surf/lh.pial:edgecolor=red ' output_folder subjects{crun} '/surf/rh.white:edgecolor=blue ' output_folder subjects{crun} '/surf/rh.pial:edgecolor=red'];
